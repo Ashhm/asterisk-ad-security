85cad2aa963d7a5a4142bf90d95b1dc0499f849a
Bulah Sergey <s.bulah.ua@gmail.com>
2017-09-20 22:09:27 +0300

    added missing config/modules

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webapck = require('webpack');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + '/public/build',
+        publicPath: "/build",
+        filename: "bundle.js"
+    },
+    devtool: 'eval-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                loader: "babel-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!autoprefixer-loader!less-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.jsx$/,
+                loader: "react-hot-loader!babel-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader"
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 25000,
+                },
+            }
+        ]
+    }
+};
